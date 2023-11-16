07b5c8c60a4edf5ac25c86a58038e384875adf68
Muhammad Usman <muhammadusmanmuzzamil@gmail.com>
2023-11-16 18:50:49 +0500

    some changings

--- task-management-app/src/Context/AuthContext.jsx
+++ task-management-app/src/Context/AuthContext.jsx
@@ -2,7 +2,6 @@
 import React, { createContext, useContext, useState } from 'react';
 
 const AuthContext = createContext();
-
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
